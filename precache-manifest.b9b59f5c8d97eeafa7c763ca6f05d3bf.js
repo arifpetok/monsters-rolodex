3dec27c1b36cd51694aea383e73aa4edf0d154bc
Arif Wijianto <arifwijianto7@gmail.com>
2019-12-24 08:34:52 +0700

    Updates

---
+++ precache-manifest.b9b59f5c8d97eeafa7c763ca6f05d3bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e66be0529ae0b0f93f3ef22584079c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7e7824201d7d200e39cc",
+    "url": "/monsters-rolodex/static/css/main.77b83cf7.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/monsters-rolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e7824201d7d200e39cc",
+    "url": "/monsters-rolodex/static/js/main.b9c85d91.chunk.js"
+  },
+  {
+    "revision": "dc25ba12107eb546721c",
+    "url": "/monsters-rolodex/static/js/runtime-main.75c8e44f.js"
+  }
+]);
